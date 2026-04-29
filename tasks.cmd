54a0ae9f23eb9adac9cbf31193e411dda7a1b7be
sembeeuk <github@sembee.co.uk>
2026-04-29 09:50:58 +0100

    Update tasks.cmd

--- tasks.cmd
+++ tasks.cmd
@@ -2,3 +2,4 @@ mkdir c:\InetPub\wwwroot\monitoring
 schtasks /create /tn "Exchange Disk Health" /tr "powershell.exe -ExecutionPolicy Bypass -File C:\Scripts\diskhealth-public.ps1" /sc minute /mo 5 /ru SYSTEM /rl HIGHEST /f
 schtasks /create /tn "Exchange Queue Health" /tr "powershell.exe -ExecutionPolicy Bypass -File C:\Scripts\queuehealth-public.ps1" /sc minute /mo 5 /ru SYSTEM /rl HIGHEST /f
 schtasks /create /tn "Exchange Backup Health" /tr "powershell.exe -ExecutionPolicy Bypass -File C:\Scripts\backuphealth-public.ps1" /sc minute /mo 5 /ru SYSTEM /rl HIGHEST /f
+schtasks /create /tn "Exchange Health Dashboard" /tr "powershell.exe -ExecutionPolicy Bypass -File C:\Scripts\exchangehealth-dashboard.ps1" /sc minute /mo 5 /ru SYSTEM /rl HIGHEST /f
